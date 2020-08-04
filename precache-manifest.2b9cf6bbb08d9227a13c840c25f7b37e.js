a67d540b19319bab968985642ca1185e5a0b247d
Shubhashis Roy Dipta <iamdipta@gmail.com>
2020-08-04 13:27:27 +0600

    Updates

---
+++ precache-manifest.2b9cf6bbb08d9227a13c840c25f7b37e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db419927ff432e944dd2e3510137ea60",
+    "url": "/messenger/index.html"
+  },
+  {
+    "revision": "2499f855aae43270ed61",
+    "url": "/messenger/static/css/main.4003332a.chunk.css"
+  },
+  {
+    "revision": "76425995959f04078b16",
+    "url": "/messenger/static/js/2.414d0609.chunk.js"
+  },
+  {
+    "revision": "e4eeb5fb0c08508c6086dd0d0f7bf4ba",
+    "url": "/messenger/static/js/2.414d0609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2499f855aae43270ed61",
+    "url": "/messenger/static/js/main.e8baaf74.chunk.js"
+  },
+  {
+    "revision": "bbeedcd0f03d0d54449a",
+    "url": "/messenger/static/js/runtime-main.494ee0d1.js"
+  }
+]);
