e8085453982a7b7bae78c84d61267471f04cdba2
Shubhashis Roy Dipta <iamdipta@gmail.com>
2020-08-04 18:53:42 +0600

    Revert "Revert "added variables react app""
    This reverts commit 744730164460d391fb08a2054c8e04afad5f074f.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API || 'http://localhost:8080'
-export const WS_URL = process.env.API || 'http://localhost:8080'
+export const API_URL = process.env.REACT_APP_API || 'http://localhost:8080'
+export const WS_URL = process.env.REACT_APP_API || 'http://localhost:8080'
