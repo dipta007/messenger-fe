517216c0517726dca0e00d88995d1ee283980dff
Shubhashis Roy Dipta <iamdipta@gmail.com>
2020-08-04 15:27:27 +0600

    added variables

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8080'
-export const WS_URL = 'http://localhost:8080'
+export const API_URL = process.env.API || 'http://localhost:8080'
+export const WS_URL = process.env.API || 'http://localhost:8080'
